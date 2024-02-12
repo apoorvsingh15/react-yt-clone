9983c5e653910c70477ac6631a304436def801df
Apoorv Singh <apoorv.singh15@gmail.com>
2024-02-12 18:49:04 +0530

    :sparkles: added dynamic data

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,33 +1,37 @@
 /* eslint-disable react/prop-types */
 import { decodeHtmlEntities } from "../utils/fetchUtils";
 import ytLogo from "/yt.svg";
-import { SignalIcon } from "@heroicons/react/24/outline";
+
 const VideoCard = ({ channelInfo }) => {
-  return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 xl:w-1/5 px-2 mb-4">
-      <div className="bg-white rounded-md">Video</div>
-      <div className="flex mt-3">
-        <div className="mr-3">
-          <div className="rounded-full bg-white">
-            <img className="h-8 w-8 " src={ytLogo} alt="youtube_Logo" />
-          </div>
-        </div>
+  if (channelInfo?.id?.kind !== "youtube#video") {
+    return <></>;
+  } else {
+    return (
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4 2xl:w-1/5 px-2 mb-12 cursor-pointer">
         <div>
-          <div className="font-medium">
-            {decodeHtmlEntities(channelInfo?.snippet?.title)}
-          </div>
-          <div>{decodeHtmlEntities(channelInfo?.snippet?.channelTitle)}</div>
-          <div>Watching</div>
-          <div className="inline-flex bg-red-900 p-1 rounded-sm">
-            <div className="mr-2">
-              <SignalIcon className="block h-4 w-4" aria-hidden="true" />
+          <img
+            className="rounded-md hover:rounded-none"
+            src={channelInfo?.snippet?.thumbnails?.medium?.url}
+          />
+        </div>
+        <div className="flex mt-3">
+          <div className="mr-3">
+            <div className="min-w-8 rounded-full bg-white">
+              <img className="h-8 w-8" src={ytLogo} alt="youtube_Logo" />
             </div>
-            <div className="text-xs">LIVE</div>
+          </div>
+          <div>
+            <div className="font-medium truncate max-w-52">
+              {decodeHtmlEntities(channelInfo?.snippet?.title)}
+            </div>
+            <div className="font-sans text-xs text-neutral-400 truncate max-w-44">
+              {decodeHtmlEntities(channelInfo?.snippet?.channelTitle)}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default VideoCard;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,31 +1,85 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import VideoCard from "../components/VideoCard";
 
 import useYoutubeSearch from "../hooks/dataFetching/YoutubeSearchHooks";
 
 const Home = () => {
-  const params = {
+  const [paginatedData, setPaginatedData] = useState([]);
+  const [params, setParams] = useState({
     part: "snippet",
     q: "f1",
+  });
+  const [isAtBottom, setIsAtBottom] = useState(false);
+
+  const { data, status } = useYoutubeSearch(params);
+
+  const paginateApiCall = () => {
+    if (data?.nextPageToken) {
+      setParams((prevParams) => ({
+        ...prevParams,
+        pageToken: data.nextPageToken,
+      }));
+    }
   };
 
-  const { data, status, error } = useYoutubeSearch(params);
+  useEffect(() => {
+    if (data?.items) {
+      let unique = new Map();
+      unique = [...paginatedData, ...data.items];
+      setPaginatedData([...unique]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.items]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      const scrollHeight =
+        document.documentElement.scrollHeight || document.body.scrollHeight;
+      const clientHeight =
+        document.documentElement.clientHeight || window.innerHeight;
+
+      // Check if the user has scrolled to the bottom
+      const isAtBottom = scrollTop + clientHeight >= scrollHeight;
+
+      setIsAtBottom(isAtBottom);
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isAtBottom && status !== "loading") paginateApiCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAtBottom]);
+
   return (
-    <>
+    <div>
       <Header />
-      {console.log(status, error, data)}
       <div className="flex m-2.5">
-        <div className="hidden sm:hidden md:hidden w-72 lg:block w-72 xl:block w-72">
+        <div className="hidden sm:hidden md:hidden min-w-72 lg:block min-w-72 xl:block min-w-72">
           Side Bar
         </div>
         <div className="flex flex-wrap -mx-2">
-          {data?.items?.length &&
-            data?.items?.map((channelInfo) => (
-              <VideoCard key={channelInfo?.etag} channelInfo={channelInfo} />
-            ))}
+          {paginatedData?.length
+            ? paginatedData?.map((channelInfo) => (
+                <VideoCard
+                  key={channelInfo?.id?.videoId}
+                  channelInfo={channelInfo}
+                />
+              ))
+            : null}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
